5fb0cf26ecb5959f1d9ade1eb6fc2195536e5818
Miguel Vivanco <djaccion@gmail.com>
2020-10-03 20:09:18 -0300

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Generamos
-echo centos > /tmp/.auth 
+echo app > /tmp/.auth 
 echo $BUILD_TAG >> /tmp/.auth 
 
 # Transifere el archivo
-scp -i /tmp/tmp/tf_pipeline/class_key.pem /tmp/.auth ubuntu@ec2-18-191-178-221.us-east-2.compute.amazonaws.com:/tmp/.auth 
-scp -i /tmp/tmp/tf_pipeline/class_key.pem /tmp/jenkins/deploy/publish ubuntu@ec2-18-191-178-221.us-east-2.compute.amazonaws.com:/tmp/publish
-ssh -i /tmp/tmp/tf_pipeline/class_key.pem ubuntu@ec2-18-191-178-221.us-east-2.compute.amazonaws.com /tmp/publish
+scp -i /tmp/tmp/tf_pipeline/class_key.pem /tmp/.auth ubuntu@ec2-3-137-174-128.us-east-2.compute.amazonaws.com:/tmp/.auth 
+scp -i /tmp/tmp/tf_pipeline/class_key.pem /tmp/jenkins/deploy/publish ubuntu@ec2-3-137-174-128.us-east-2.compute.amazonaws.com:/tmp/publish
+ssh -i /tmp/tmp/tf_pipeline/class_key.pem ubuntu@ec2-3-137-174-128.us-east-2.compute.amazonaws.com /tmp/publish
