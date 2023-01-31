9182c79d9715fb2fef76dcdcf844b7c261e28b68
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-01-31 12:32:08 -0300

    Update push.sh

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -3,7 +3,7 @@ export BUILD_TAG=12
 echo "########################"
 echo "*** Preparing to push ***"
 echo "########################"
-REGISTRY="ec2-3-137-174-128.us-east-2.compute.amazonaws.com:5000"
+REGISTRY="https://registry.hub.docker.com/aaronbarra"
 IMAGE="app" 
 echo "*** Tagging image ***"
 docker tag $IMAGE:$BUILD_TAG $REGISTRY/$IMAGE:$BUILD_TAG
