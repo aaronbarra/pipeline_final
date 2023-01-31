e20df412fef29a8c9e6fa5d6fec44fffc9c7c8fa
aaronbarra <117938545+aaronbarra@users.noreply.github.com>
2023-01-31 18:19:23 -0300

    Update push.sh

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -3,11 +3,10 @@ echo "########################"
 echo "*** Preparing to push ***"
 echo "########################"
 REGISTRY="registry.hub.docker.com"
-USERNAME=aaronbarra
 IMAGE="app"
 BUILD_ID=$BUILD_ID
 echo "*** Tagging image ***"
 docker tag $IMAGE:$BUILD_ID $REGISTRY/$IMAGE:$BUILD_ID
 echo "*** Pushing image ***"
-docker login -u aaronbarra --password-stdin karina3008. -server docker.io
-docker push $USERNAME/$IMAGE:$BUILD_ID
+docker login -u aaronbarra --password-stdin karina3008. 
+docker push $REGISTRY/$IMAGE:$BUILD_ID
